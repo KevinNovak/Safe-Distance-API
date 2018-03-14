4f83ad8abc933f41aca7c9d035dcb67869662891
Kevin Novak <kevinmnovak@yahoo.com>
2018-03-13 21:12:01 -0400

    Added /api/stats route

--- app.js
+++ app.js
@@ -20,7 +20,34 @@ app.get('/', function (request, response) {
     response.json(body);
 });
 
+// GET /api/stats
+app.get('/api/stats', function (request, response) {
+    // Fake it til ya make it
+    var body = [
+        {
+            id: 1,
+            speed: 30,
+            distance: 50,
+            time: "2018-03-14T01:03:28.034234Z"
+        },
+        {
+            id: 2,
+            speed: 30,
+            distance: 50,
+            time: "2018-03-14T01:03:28.034234Z"
+        },
+        {
+            id: 3,
+            speed: 30,
+            distance: 50,
+            time: "2018-03-14T01:03:28.034234Z"
+        }
+    ];
+    response.status(200);
+    response.json(body);
+});
+
 // Run app
 app.listen(PORT, function () {
-    console.log("Server running on port " + PORT);
+    console.log(`Server running on port ${PORT}`);
 });
